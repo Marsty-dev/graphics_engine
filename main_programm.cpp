8994fde1d33ca47da7abba6f48935e25631094e9
Marsty-dev <maksym.rogovik@gmail.com>
2025-02-09 05:09:21 +0100

    Update of main menu

--- main_programm.cpp
+++ main_programm.cpp
@@ -15,13 +15,24 @@ void main_programm()
 
     DrawRectangle(50, 550, button_main_w, button_main_h, GREEN);
     DrawText("Pyramid", 80, 560, 30, WHITE);
+    if(GetMouseX() > 50 && GetMouseX() < 250 && GetMouseY() > 550 && GetMouseY() < 600){
+        DrawRectangle(50, 550, button_main_w, button_main_h, DARKGREEN);
+        DrawText("Pyramid", 80, 560, 30, WHITE);
+    }
 
     DrawRectangle(300, 550, button_main_w, button_main_h, BLUE);
     DrawText("Sphere", 340, 560, 30, WHITE);
+    if(GetMouseX() > 300 && GetMouseX() < 500 && GetMouseY() > 550 && GetMouseY() < 600){
+        DrawRectangle(300, 550, button_main_w, button_main_h, DARKBLUE);
+        DrawText("Sphere", 340, 560, 30, WHITE);
+    }
 
     DrawRectangle(550, 550, button_main_w, button_main_h, RED);
     DrawText("Cube", 600, 560, 30, WHITE);
-
+    if(GetMouseX() > 550 && GetMouseX() < 750 && GetMouseY() > 550 && GetMouseY() < 600){
+        DrawRectangle(550, 550, button_main_w, button_main_h, ORANGE);
+        DrawText("Cube", 600, 560, 30, WHITE);
+    }
 
     EndDrawing();
 }
